afb774e51693c61ab3010058a1d105a4002f57b7
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2021-08-19 22:07:05 +0900

    Fix : 챕터 1 파일 복구

--- src/ch1/ch1_ reform.ts
+++ src/ch1/ch1_ reform.ts
@@ -1,94 +1,114 @@
 {
+  // 자율성 높이기
   class Invitation {
     constructor(private _when: Date) {}
   }
 
   class Ticket {
-    constructor(private _fee: bigint) {}
-    get fee(): bigint {
-      return this._fee
+    constructor(private _fee: number) {}
+
+    get fee(): number {
+      return this._fee;
     }
   }
 
   class Bag {
-    private _amount: bigint;
+    private _amount: number;
     private _invitation: Invitation;
     private _ticket: Ticket;
-
-    constructor(amount: bigint, invitation: Invitation, ticket: Ticket) {
+    constructor(amount: number, invitation: Invitation, ticket: Ticket) {
       this._amount = amount;
       this._invitation = invitation;
-      this._ticket = ticket
+      this._ticket = ticket;
     }
 
-    set ticket (ticket: Ticket) {
-      this._ticket = ticket
-    }
+    plusAmount = (amount: number): void => {
+      this._amount += amount;
+    };
 
-    hasInvitation = (): boolean => {
+    private minusAmount = (amount: number): void => {
+      this._amount -= amount;
+    };
+
+    private setTicket = (ticket: Ticket): void => {
+      this._ticket = ticket;
+    };
+
+    hold = () => {
+      if (this.hasInvitation) {
+        this.setTicket(this._ticket);
+        return 0;
+      } else {
+        this.minusAmount(this._ticket.fee);
+        this.setTicket(this._ticket);
+        return this._ticket.fee;
+      }
+    };
+
+    private get hasInvitation(): boolean {
       return !!this._invitation;
     }
 
-    hasTicket = (): boolean => {
-      return !!this._ticket
-    }
-
-    minusAmount = (amount: bigint): void => {
-      this._amount -= amount;
-    }
-
-    plusAmount = (amount: bigint): void => {
-      this._amount += amount;
-    }
-
-    hold = (ticket: Ticket): bigint => {
-      if (this.hasInvitation()) {
-        this.ticket = ticket
-        return BigInt(0)
-      } else {
-        this.ticket = ticket
-        this.minusAmount(ticket.fee);
-        return ticket.fee
-      }
+    get hasTicket(): boolean {
+      return !!this._ticket;
     }
   }
 
   class Audience {
     constructor(private _bag: Bag) {}
-    
-    get bag():Bag {
-      return this._bag;
-    }
 
-    buy = (ticket : Ticket): bigint => this.bag.hold(ticket);
+    buy = (ticket: Ticket): number => {
+      return this._bag.hold();
+    };
   }
 
   class TicketOffice {
-    constructor(private _amount:bigint, private _tickets: Array<Ticket>) {}
+    constructor(private _amount: number, private _tickets: Array<Ticket>) {}
 
-    getTicket = (): Ticket => {
+    getTicket() {
       const result = this._tickets[0];
-      this._tickets.shift()
+      this._tickets = this._tickets.filter(
+        (ticket, index: number) => 0 !== index
+      );
       return result;
     }
 
-    minusAmount = (amount: bigint): bigint => this._amount - amount;
-    plusAmount = (amount: bigint): bigint => this._amount + amount
+    minusAmount(amount: number) {
+      this._amount -= amount;
+    }
+
+    plusAmount(amount: number) {
+      this._amount += amount;
+    }
+
+    // sellTicketTo= (audience: Audience) => {
+    // 이렇게 하면 TicketOffice에 대한 응집도, 자율성은 높아지지만
+    // Audience에 대한 의존성이 추가되어 전체적인 관점에서 결합도가 높아진다.
+    // 회의 결과 TicketOffice의 응집도를 낮추고
+    // Tickeseller를 그대로 두어 전체적인 결합도를 낮추기로 하였다.
+    //    const ticket: Ticket = this.getTicket();
+    //     this.plusAmount(audience.buy(ticket))
+    // }
   }
 
   class TicketSeller {
     constructor(private _ticketOffice: TicketOffice) {}
 
+    // sellTo = (audience:Audience) => {
+    //     this._ticketOffice.sellTicketTo(audience)
+    // }
+
     sellTo = (audience: Audience) => {
-        this._ticketOffice.plusAmount(audience.buy(this._ticketOffice.getTicket()))
-    }
+      const ticket: Ticket = this._ticketOffice.getTicket();
+      this._ticketOffice.plusAmount(audience.buy(ticket));
+    };
   }
 
   class Theater {
     constructor(private _ticketSeller: TicketSeller) {}
 
-    enter = (audience: Audience): void => {
+    enter = (audience: Audience) => {
       this._ticketSeller.sellTo(audience);
-    }
+    };
   }
 }
--- src/ch1/ch1_init.ts
+++ src/ch1/ch1_init.ts
@@ -4,70 +4,70 @@
   }
 
   class Ticket {
-    constructor(private _fee: bigint) {}
-    get fee(): bigint {
-      return this._fee
+    constructor(private _fee: number) {}
+
+    get fee(): number {
+      return this._fee;
     }
   }
 
   class Bag {
-    private _amount: bigint;
+    private _amount: number;
     private _invitation: Invitation;
     private _ticket: Ticket;
-
-    // Q. constructor는 amount만 들어올때랑 invitation & amount 들어올 때 둘 다 적용 필요
-    constructor(amount: bigint, invitation: Invitation, ticket: Ticket) {
+    constructor(amount: number, invitation: Invitation, ticket: Ticket) {
       this._amount = amount;
       this._invitation = invitation;
-      this._ticket = ticket
+      this._ticket = ticket;
     }
 
-    // Q. 변수에 대한 값이 아닌데, get이 맞는것인가?
-    hasInvitation = (): boolean => {
-      // Q. null에 대한 체크는 모든 falsy한 값을 체크해야 하는가? null만 체크해야 하는가?
+    plusAmount = (amount: number): void => {
+      this._amount += amount;
+    };
+
+    minusAmount = (amount: number): void => {
+      this._amount -= amount;
+    };
+
+    setTicket = (ticket: Ticket): void => {
+      this._ticket = ticket;
+    };
+
+    get hasInvitation(): boolean {
       return !!this._invitation;
     }
 
-    hasTicket = (): boolean => {
-      return !!this._ticket
-    }
-
-    // C. set으로 해줘야 되는 부분인데...
-    // setTicket = (ticket: Ticket): void => {
-    //   this._ticket = ticket
-    // }
-    set ticket (ticket: Ticket) {
-      this._ticket = ticket
-    }
-
-    minusAmount = (amount: bigint): void => {
-      this._amount -= amount;
-    }
-
-    plusAmount = (amount: bigint): void => {
-      this._amount += amount;
+    get hasTicket(): boolean {
+      return !!this._ticket;
     }
   }
 
   class Audience {
     constructor(private _bag: Bag) {}
-    
-    get bag():Bag {
+
+    get bag(): Bag {
       return this._bag;
     }
   }
 
   class TicketOffice {
-    constructor(private _amount:bigint, private _tickets: Array<Ticket>) {}
+    constructor(private _amount: number, private _tickets: Array<Ticket>) {}
 
-    getTicket = (): Ticket => {
+    getTicket() {
       const result = this._tickets[0];
-      this._tickets.shift()
+      this._tickets = this._tickets.filter(
+        (ticket, index: number) => 0 !== index
+      );
       return result;
     }
 
-    minusAmount = (amount: bigint): bigint => this._amount - amount;
-    plusAmount = (amount: bigint): bigint => this._amount + amount
+    minusAmount(amount: number) {
+      this._amount -= amount;
+    }
+
+    plusAmount(amount: number) {
+      this._amount += amount;
+    }
   }
 
   class TicketSeller {
@@ -81,16 +81,16 @@
   class Theater {
     constructor(private _ticketSeller: TicketSeller) {}
 
-    enter = (audience: Audience): void => {
-      if (audience.bag.hasInvitation()) {
+    enter = (audience: Audience) => {
+      if (audience.bag.hasInvitation) {
         const ticket: Ticket = this._ticketSeller.ticketOffice.getTicket();
-        audience.bag.ticket = ticket;
+        audience.bag.setTicket(ticket);
       } else {
         const ticket: Ticket = this._ticketSeller.ticketOffice.getTicket();
         audience.bag.minusAmount(ticket.fee);
         this._ticketSeller.ticketOffice.plusAmount(ticket.fee);
-        audience.bag.ticket = ticket;
+        audience.bag.setTicket(ticket);
       }
-    }
+    };
   }
 }
